1e1740ec1bde13c1d77fd9df41c5cc3585f087f3
faoakam-prog <faoakam@gmail.com>
2026-06-04 11:17:30 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE='finance3-v1';
+const CACHE='finance3-v2';
 const FILES=['/','/manifest.json','/icon-192.png','/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
